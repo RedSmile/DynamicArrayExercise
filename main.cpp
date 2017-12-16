40e8cc7618fd0f8075db96a56b32824ee1415606
RedSmile <34597036+RedSmile@users.noreply.github.com>
2017-12-16 16:41:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,30 @@
-//alcuni termini informatici in italiano potrebbero esserew sbagliati, tradurre dal russo non e' semplice
+//alcuni termini informatici in italiano potrebbero essere sbagliati, tradurre dal russo non e' semplice
 //non vi aspettate grandi cose e' solo un esercizio sugli array dinamici 
-
 
 #include <iostream>
 #include <ctime>
 
-
 using namespace std;
 
-void clean() //Pulisce la console chiedendo l'input all'utente
+//Pulisce la console chiedendo l'input all'utente
+void clean() 
 {
 	system("pause");
 	system("cls");
 }
 
-
-void Arr(const int size, int *arr) //Riempe l'array e mette in output numero contenuto e indicatore
+//Riempe l'array e mette in output numero contenuto e indicatore
+void Arr(const int size, int *arr) 
 {
 	for (int i = 0; i < size; i++)
 	{
 		arr[i] = rand() % 99 + 1;
 	}
-
 	for (int i = 0; i < size; i++)
 	{
 		cout << "il contenuto della casella n." << i << " e':\t" << arr[i] << endl;
 	}
-
 	cout << "============================================================================================================" << endl;
-
 	for (int i = 0; i < size; i++)
 	{
 		cout << "il pointer del numero " << arr[i] << " nella casella " << i << " e':\t" << &arr[i] << endl;
@@ -36,8 +32,8 @@ void Arr(const int size, int *arr) //Riempe l'array e mette in output numero con
 	}
 }
 
-
-void AddNewArr(int size,const int newsize, int *arr) //Riempe il nuovo array e mette in output numero contenuto e indicatore
+//Riempe il nuovo array e mette in output numero contenuto e indicatore
+void AddNewArr(int size,const int newsize, int *arr) 
 {
 	int *newarr = new int[newsize];
 
@@ -45,28 +41,21 @@ void AddNewArr(int size,const int newsize, int *arr) //Riempe il nuovo array e m
 	{
 		newarr[i] = arr[i];
 	}
-	
 	delete[] arr;
-	
 	for ( ; size < newsize; )
 	{
 		newarr[size] = rand() % 99 + 1;
 		size++;
 	}
-
 	for (int i = 0; i < newsize; i++)
 	{
 		cout << "il contenuto nell'array della casella n. " << i << " e':\t" << newarr[i] << endl;
 	}
-
 	cout << "============================================================================================================" << endl;
-	
 	for (int i = 0; i < newsize; i++)
 	{
 		cout << "il pointer del numero " << newarr[i] << " nella casella " << i << " e':\t" << &newarr[i] << endl;
-	
 	}
-
 	delete[] newarr;
 }
 
@@ -84,38 +73,27 @@ int main()
 
 	cout << "inserisci la grandezza dell'array" << endl;
 	cin >> size;
-
 	int *arr = new int[size];
-
 	clean();
-
 	cout << "============================================================================================================" << endl;
-
-	Arr(size, arr); //Richiamo alla funzione che riempe l'array e mette in output numero contenuto e indicatore
-
+	//Richiamo alla funzione che riempe l'array e mette in output numero contenuto e pointer
+	Arr(size, arr); 
 	cout << "============================================================================================================" << endl;
-
 	clean();
-
 	cout << "vuoi aggiungere un numero all'array?\ny/n" << endl;
 	cin >> risposta;
 	system("cls");
-
 	if (risposta == 'y' || risposta == 'Y')
 	{
 		cout << "di quanto vuoi aumentare l'array?" << endl;
 		cin >> add;
-
 		newsize = size + add;
-
 		cout << "============================================================================================================" << endl;
-
-		AddNewArr(size, newsize, arr); //Richiamo alla funzione che riempe il nuovo array e mette in output numero contenuto e indicatore
-
+		//Richiamo alla funzione che riempe il nuovo array e mette in output numero contenuto e pointer
+		AddNewArr(size, newsize, arr); 
 		cout << "============================================================================================================" << endl;
 		clean();
 	}
-	
 	cout << "Programma creato da RedSmile :D" << endl;
 
 	system("pause");
