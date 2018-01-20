3dac83e0746c311ce98db0c648f9710d56c3b08d
RedSmile <34597036+RedSmile@users.noreply.github.com>
2018-01-20 14:10:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 //non vi aspettate grandi cose e' solo un esercizio sugli array dinamici 
 
 #include <iostream>
@@ -7,55 +6,46 @@
 using namespace std;
 
 //Pulisce la console chiedendo l'input all'utente
-void clean() 
-{
+void clean(){
 	system("pause");
 	system("cls");
 }
 
 //Riempe l'array e mette in output numero contenuto e indicatore
-void Arr(const int size, int *arr) 
+void Arr(const int size, int *arr)
 {
 	for (int i = 0; i < size; i++)
-	{
 		arr[i] = rand() % 99 + 1;
-	}
+
 	for (int i = 0; i < size; i++)
-	{
 		cout << "il contenuto della casella n." << i << " e':\t" << arr[i] << endl;
-	}
 	cout << "============================================================================================================" << endl;
-	for (int i = 0; i < size; i++)
-	{
+
+	for (int i = 0; i < size; i++){
 		cout << "il pointer del numero " << arr[i] << " nella casella " << i << " e':\t" << &arr[i] << endl;
 		&arr[i];
 	}
 }
 
 //Riempe il nuovo array e mette in output numero contenuto e indicatore
-void AddNewArr(int size,const int newsize, int *arr) 
-{
+void AddNewArr(int size, const int newsize, int *arr){
 	int *newarr = new int[newsize];
 
 	for (int i = 0; i < size; i++)
-	{
 		newarr[i] = arr[i];
-	}
 	delete[] arr;
-	for ( ; size < newsize; )
-	{
+	for (; size < newsize; ){
 		newarr[size] = rand() % 99 + 1;
 		size++;
 	}
+
 	for (int i = 0; i < newsize; i++)
-	{
 		cout << "il contenuto nell'array della casella n. " << i << " e':\t" << newarr[i] << endl;
-	}
 	cout << "============================================================================================================" << endl;
+
 	for (int i = 0; i < newsize; i++)
-	{
 		cout << "il pointer del numero " << newarr[i] << " nella casella " << i << " e':\t" << &newarr[i] << endl;
-	}
+
 	delete[] newarr;
 }
 
@@ -67,7 +57,7 @@ int main()
 	clean();
 
 	srand(time(NULL)); //rendomizzatore
-	
+
 	char risposta;
 	int size, newsize, add, remove;
 
@@ -77,7 +67,7 @@ int main()
 	clean();
 	cout << "============================================================================================================" << endl;
 	//Richiamo alla funzione che riempe l'array e mette in output numero contenuto e pointer
-	Arr(size, arr); 
+	Arr(size, arr);
 	cout << "============================================================================================================" << endl;
 	clean();
 	cout << "vuoi aggiungere un numero all'array?\ny/n" << endl;
@@ -90,7 +80,7 @@ int main()
 		newsize = size + add;
 		cout << "============================================================================================================" << endl;
 		//Richiamo alla funzione che riempe il nuovo array e mette in output numero contenuto e pointer
-		AddNewArr(size, newsize, arr); 
+		AddNewArr(size, newsize, arr);
 		cout << "============================================================================================================" << endl;
 		clean();
 	}
